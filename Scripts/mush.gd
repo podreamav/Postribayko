e5388ef89c53795d9fa2e80e23c2f394d0f95b72
dreamay <quentinmelonn@gmail.com>
2024-06-02 15:45:02 +0300

    added new textures "grass" and "house". fixed the "attack" animation of mush

--- Scripts/mush.gd
+++ Scripts/mush.gd
@@ -11,75 +11,59 @@ var player_in_area = false
 var mush_is_attacking = false
 
 func play_attack():
+	$detection_area/CollisionShape3D.disabled = true
 	mush_is_attacking = true
-	await get_tree().create_timer(1.4583).timeout
-	
+	# Play the attack animation and wait for it to finish
+	$mush_giant/AnimationPlayer.play("Attack")
+	await $mush_giant/AnimationPlayer.animation_finished
 	mush_is_attacking = false
-	#print("hello from attack func")
-	#$mush_giant/AnimationPlayer.play("Attack")
+	$detection_area/CollisionShape3D.disabled = false
 
 func _ready():
 	dead = false
-	
+
 func _physics_process(delta):
-	#if player_in_area and !dead:
-		#var direction = (player.global_transform.origin - global_transform.origin).normalized()
-		#direction.y += push_height
-		#player.velocity += direction * push_strength
 	if !dead:
+		if mush_is_attacking:
+			return  # Skip the rest of the logic if attacking
+		
 		$detection_area/CollisionShape3D.disabled = false
 		move_and_slide()
-		velocity.y -= gravity * delta
 		
 		if not is_on_floor():
 			velocity.y -= gravity * delta
-			
-		#else:
-			#velocity.y = 0
+
 		if player_in_area:
 			var direction = (player.position - position).normalized()
 			direction.y = 0 # Ignore the Y-axis for horizontal movement
 			position += direction / speed
-			#$mush_giant/AnimationPlayer.play("Run")
+			$mush_giant/AnimationPlayer.play("Run")
 			look_at(Vector3(player.position.x, position.y, player.position.z))
-		elif mush_is_attacking:
-			$mush_giant/AnimationPlayer.play("Attack")
 		else:
-			#stop_movement()
 			$mush_giant/AnimationPlayer.play("Idle")
 	else:
-		$detection_area.disabled = true
+		$detection_area/CollisionShape3D.disabled = true
 
 func _on_detection_area_body_entered(body):
 	if body.has_method("player"):
 		player_in_area = true
 		player = body
-		
-		
 
 func _on_detection_area_body_exited(body):
 	if body.has_method("player"):
 		player_in_area = false
 
-
 func _on_hitbox_area_entered(area):
 	var damage
-	#if area.has_method("mush1"):
+	#if area.has_method("legs"):
 		#damage = 1
 		#take_damage(damage)
-		
-
-#func attack_anim(body):
-	#if body.has_method("mush"):
-		#attacking = true
-		#if attacking == true:
-			#$mush_giant/AnimationPlayer.play("Attack")
 
 func take_damage(damage):
 	health = health - damage
 	if health <= 0 and !dead:
 		death()
-	
+
 func death():
 	dead = true
 	$hitbox/CollisionShape3D.disabled = true
@@ -87,12 +71,6 @@ func death():
 	$mush_giant/AnimationPlayer.play("death")
 	await $mush_giant/AnimationPlayer.animation_finished
 	queue_free()
-	
-#func stop_movement():
-	## Implement logic to stop the mush
-	#velocity = Vector3.ZERO
-	#move_and_slide()
-
 
 func mush():
 	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,28 +1,18 @@
 extends CharacterBody3D
-
 @onready var camera = $Camera
 @onready var armature = $FlowerKnight/Armature
 @onready var anim_tree = $AnimationTree
-
-signal player_damaged
-
-const SPEED = 3.0
-const JUMP_VELOCITY = 4.2
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 var health = 6
 var dead = false
-var push_strength : float = 7
-var push_height : float = 0.2
-var push_length : float = 5
-
-
+var push_strength : float = 4
+var push_height : float = 0.4
+var push_length : float = 7
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-
-
 func _ready():
 	dead = false
-
 	$Health/DeadHeart.visible = false
 	$Health/DeadHeart2.visible = false
 	$Health/DeadHeart3.visible = false
@@ -33,16 +23,13 @@ func _ready():
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
-
 func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -56,24 +43,20 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp (velocity.x, 0.0, 0.25)
 		velocity.z = lerp (velocity.z, 0.0, 0.25)
-
 	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
-
 	move_and_slide()
 
 	# Camera is smoothly moving along player
 	$Camera.position = lerp($Camera.position, position, 0.15)
-
-
+	
 func player():
 	pass
-
 func _on_hitbox_body_entered(body):
 	if body.has_method("mush"):
 		var mush = body
 		mush.play_attack()
-		
-		
+
+
 		# Calculate direction vector from mush to player
 		var direction = (global_transform.origin - mush.global_transform.origin).normalized()
 
@@ -81,33 +64,28 @@ func _on_hitbox_body_entered(body):
 		direction.y += push_height
 		direction.x += push_length * sign(direction.x)
 		direction.z += push_length * sign(direction.z)
-
 		# Apply push to player's velocity
 		velocity += direction * push_strength
-
 		var damage = 1
 		take_damage(damage)
+		
+		
 	
 	if body.has_method("apply_push"):
 		# Calculate direction vector from player to block
 		var direction = (body.global_transform.origin - global_transform.origin).normalized()
-
 		# Add push in both height and length
 		direction.y += push_height
 		if abs(direction.x) > abs(direction.z):
 			direction.x += push_length * sign(direction.x)
 		else:
 			direction.z += push_length * sign(direction.z)
-
 		# Apply push to block
 		body.apply_push(direction)
-
 		
 func take_damage(damage):
 	health = health - damage
 	print (health)
-	get_node("mush_giant")
-	emit_signal("player_damaged")
 	
 	if health == 5:
 		$Health/HalfHeart3.visible = true
@@ -124,14 +102,6 @@ func take_damage(damage):
 	
 	if health <= 0 and !dead:
 		death()
-
-
-
-
 func death():
 	dead = true
-	get_tree().change_scene_to_file("res://Scenes/intro.tscn")
-	
-
-
-		
+	#get_tree().change_scene_to_file("res://Scenes/intro.tscn")
